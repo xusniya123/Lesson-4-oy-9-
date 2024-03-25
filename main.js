146dad6a2309ec243489ad5c7cefb2da13962a03
xusniya123 <xusniya12345678@gmail.com>
2024-03-25 16:20:51 +0500

    Lesson 4 moth (9)

---
+++ main.js
@@ -0,0 +1,27 @@
+
+// const array = [1, 2, 3, 4, 5, ];
+// const array2 = ["BMW", "Mersedens", "Tesla", "Gelik"];
+// for (const element of array ){
+//   array2.push(element);
+// }
+// console.log(array);
+// console.log(array2);
+
+
+
+let array = ["non", "aziza", ];
+let juft = ["mashina", "gul", "olma"]
+for (let i = 0; i< array.length; i ++){
+    let check = array [i] % 2;
+    if (check === 0){
+        juft.push(array[i])
+    }
+    
+}
+
+console.log(array ,"palindrom sozli massiv");
+console.log(juft ,"palindrom bo'lmagan sozli massiv" );
+
+
+
+
